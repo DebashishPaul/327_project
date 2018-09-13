95da9c6070578f4f0e204f48940c085f1c0db28b
Debashish Paul <debashish333paul@gmail.com>
2018-09-13 13:00:13 +0600

    slider

--- index.php
+++ index.php
@@ -110,32 +110,212 @@
 
                             <!-- Start WOWSlider.com BODY section -->
                             <!-- add to the <body> of your page -->
-                            <div id="wowslider-container0">
-                                <div class="ws_images">
-                                    <ul>
-                                        <li><img src="image/940379.jpg" alt="Mental Health Counseling" title="Mental Health Counseling" id="wows0_0" /></li>
-                                        <li><a href="http://wowslider.net"><img src="motivational-hd-images.jpg" alt="image slider" title="Psychiatry, Psychology, Counseling, and Therapy" id="wows0_1"/></a></li>
-                                        <li><img src="maxresdefault.jpg" alt="Counseling & Psychological Services" title="Counseling & Psychological Services" id="wows0_2" />
-                                        </li>
-                                        <li><img src="thumb-1920-168908.jpg" alt="Mental Health Counseling" title="Mental Health Counseling" id="wows0_0" /></li>
 
-                                    </ul>
-                                </div>
-                                <!--
-                            <div class="ws_bullets">
-                                <div>
-                                    <a href="#" title="Mental Health Counseling"><span><img src="image/940379.jpg" alt="Mental Health Counseling"/>1</span></a>
-                                    <a href="#" title="Psychiatry, Psychology, Counseling, and Therapy"><span><img src="motivational-hd-images.jpg" alt="Psychiatry, Psychology, Counseling, and Therapy"/>2</span></a>
-                                    <a href="#" title="Counseling & Psychological Services"><span><img src="maxresdefault.jpg" alt="Counseling & Psychological Services"/>3</span></a>
-                                    <a href="#" title="Counseling & Psychological Services"><span><img src="thumb-1920-168908.jpg" alt="Counseling & Psychological Services"/>3</span></a>
-                                </div>
-                            </div>
--->
-                                <div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.net">css image gallery</a> by WOWSlider.com v8.8</div>
-                                <div class="ws_shadow"></div>
-                            </div>
-                            <script type="text/javascript" src="engine0/wowslider.js"></script>
-                            <script type="text/javascript" src="engine0/script.js"></script>
+
+                            <script type="text/javascript">
+                                //Specify the slider's width (in pixels)
+
+                                var sliderwidth = "1297px"
+
+                                //Specify the slider's height
+
+                                var sliderheight = "400px"
+
+                                //Specify the slider's slide speed (larger is faster 1-10)
+
+
+
+                                var slidespeed = 6
+
+                                //configure background color:
+
+                                slidebgcolor = "#D6D3D6"
+
+
+
+                                //Specify the slider's images
+
+                                var leftrightslide = new Array()
+
+                                var finalslide = ''
+
+
+
+                                leftrightslide[1] = '<img src="images/11.jpg">'
+                                leftrightslide[2] = '<img src="images/12.jpg"> </a>'
+                                leftrightslide[6] = '<img src="images/13.jpg">'
+                                leftrightslide[5] = '<img src="images/14.jpg">'
+                                leftrightslide[0] = '<img src="images/15.jpg">'
+                                leftrightslide[3] = '<img src="images/16.jpg">'
+                                leftrightslide[4] = '<img src="images/17.jpg">'
+
+
+
+
+
+
+
+
+
+                                //Specify gap between each image (use HTML):
+
+                                var imagegap = " "
+
+
+
+                                //Specify pixels gap between each slideshow rotation (use integer):
+
+                                var slideshowgap = 5
+
+
+
+                                ////NO NEED TO EDIT BELOW THIS LINE////////////
+
+
+
+                                var copyspeed = slidespeed
+
+                                leftrightslide = '<nobr>' + leftrightslide.join(imagegap) + '</nobr>'
+
+                                var iedom = document.all || document.getElementById
+
+                                if (iedom)
+
+                                    document.write('<span id="temp" style="visibility:hidden;position:absolute;top:-100px;left:-9000px">' + leftrightslide + '</span>')
+
+                                var actualwidth = ''
+
+                                var cross_slide, ns_slide
+
+
+
+                                function fillup() {
+
+                                    if (iedom) {
+
+                                        cross_slide = document.getElementById ? document.getElementById("test2") : document.all.test2
+
+                                        cross_slide2 = document.getElementById ? document.getElementById("test3") : document.all.test3
+
+                                        cross_slide.innerHTML = cross_slide2.innerHTML = leftrightslide
+
+                                        actualwidth = document.all ? cross_slide.offsetWidth : document.getElementById("temp").offsetWidth
+
+                                        cross_slide2.style.left = actualwidth + slideshowgap + "px"
+
+                                    } else if (document.layers) {
+
+                                        ns_slide = document.ns_slidemenu.document.ns_slidemenu2
+
+                                        ns_slide2 = document.ns_slidemenu.document.ns_slidemenu3
+
+                                        ns_slide.document.write(leftrightslide)
+
+                                        ns_slide.document.close()
+
+                                        actualwidth = ns_slide.document.width
+
+                                        ns_slide2.left = actualwidth + slideshowgap
+
+                                        ns_slide2.document.write(leftrightslide)
+
+                                        ns_slide2.document.close()
+
+                                    }
+
+                                    lefttime = setInterval("slideleft()", 30)
+
+                                }
+
+                                window.onload = fillup
+
+
+
+                                function slideleft() {
+
+                                    if (iedom) {
+
+                                        if (parseInt(cross_slide.style.left) > (actualwidth * (-1) + 8))
+
+                                            cross_slide.style.left = parseInt(cross_slide.style.left) - copyspeed + "px"
+
+                                        else
+
+                                            cross_slide.style.left = parseInt(cross_slide2.style.left) + actualwidth + slideshowgap + "px"
+
+
+
+                                        if (parseInt(cross_slide2.style.left) > (actualwidth * (-1) + 8))
+
+                                            cross_slide2.style.left = parseInt(cross_slide2.style.left) - copyspeed + "px"
+
+                                        else
+
+                                            cross_slide2.style.left = parseInt(cross_slide.style.left) + actualwidth + slideshowgap + "px"
+
+
+
+                                    } else if (document.layers) {
+
+                                        if (ns_slide.left > (actualwidth * (-1) + 8))
+
+                                            ns_slide.left -= copyspeed
+
+                                        else
+
+                                            ns_slide.left = ns_slide2.left + actualwidth + slideshowgap
+
+
+
+                                        if (ns_slide2.left > (actualwidth * (-1) + 8))
+
+                                            ns_slide2.left -= copyspeed
+
+                                        else
+
+                                            ns_slide2.left = ns_slide.left + actualwidth + slideshowgap
+
+                                    }
+
+                                }
+
+                                if (iedom || document.layers) {
+                                    with(document) {
+
+                                        document.write('<table border="0" cellspacing="0" cellpadding="0"><td>')
+
+                                        if (iedom) {
+
+                                            write('<div style="position:relative;width:' + sliderwidth + ';height:' + sliderheight + ';overflow:hidden">')
+
+                                            write('<div style="position:absolute;width:' + sliderwidth + ';height:' + sliderheight + ';background-color:' + slidebgcolor + '" onMouseover="copyspeed=0" onMouseout="copyspeed=slidespeed">')
+
+                                            write('<div id="test2" style="position:absolute;left:0px;top:0px"></div>')
+
+                                            write('<div id="test3" style="position:absolute;left:-1000px;top:0px"></div>')
+
+                                            write('</div></div>')
+
+                                        } else if (document.layers) {
+
+                                            write('<ilayer width=' + sliderwidth + ' height=' + sliderheight + ' name="ns_slidemenu" bgColor=' + slidebgcolor + '>')
+
+                                            write('<layer name="ns_slidemenu2" left=0 top=0 onMouseover="copyspeed=0" onMouseout="copyspeed=slidespeed"></layer>')
+
+                                            write('<layer name="ns_slidemenu3" left=0 top=0 onMouseover="copyspeed=0" onMouseout="copyspeed=slidespeed"></layer>')
+
+                                            write('</ilayer>')
+
+                                        }
+
+                                        document.write('</td></table>')
+
+                                    }
+                                }
+
+                            </script>
+
+
                             <!-- End WOWSlider.com BODY section -->
                             <!-- /.navbar-collapse -->
                         </div>
